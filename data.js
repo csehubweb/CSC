1f1e3b7b00cc7d8d7fcf8b4aacf62d35cf21347c
csehubweb <thirdidbgmi2026@gmail.com>
2026-08-23 11:35:42 +0530

    V3

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const initialWebsites = [
         id: "1",
         title: "LeetCode (Problem Solving & DSA)",
         url: "https://leetcode.com/",
-        category: "coding",
+        category: "pan",
         description: "Platform for preparing technical coding interviews and practicing DSA problems.",
         tags: ["DSA", "Interviews", "Coding"],
         isNew: true,
